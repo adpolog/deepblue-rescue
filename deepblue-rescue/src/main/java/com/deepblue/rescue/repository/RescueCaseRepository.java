072b9736ec8364a71ae170a8ae1fa1be5c6e61c1
daramosh <daramosh@unimagdalena.edu.co>
2026-09-10 23:34:59 -0500

    30 al 39

---
+++ deepblue-rescue/src/main/java/com/deepblue/rescue/repository/RescueCaseRepository.java
@@ -0,0 +1,14 @@
+// Archivo 2: RescueCaseRepository.java (Paso 32)
+package com.deepblue.rescue.repository;
+
+import com.deepblue.rescue.domain.RescueCase;
+import com.deepblue.rescue.domain.RescueStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RescueCaseRepository extends JpaRepository<RescueCase, Long> {
+    Optional<RescueCase> findByCaseCode(String caseCode);
+    List<RescueCase> findByStatusOrderByRescueDateAsc(RescueStatus status);
+}
